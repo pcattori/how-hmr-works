51df6ae640711c2ad4a6c457a3e718f17ebdd037
Pedro Cattori <pcattori@gmail.com>
2022-11-28 23:31:59 -0500

    implement `serve` command

--- cli/serve.ts
+++ cli/serve.ts
@@ -1,3 +1,11 @@
+import express from 'express'
+
+import * as Paths from './utils/paths'
+
+let PORT = 3001
+
 export default async () => {
-  console.log('serve')
+  let app = express()
+  app.use('/', express.static(Paths.distDir))
+  app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
 }
